e215ce3b4e5c6551582a3292e39fe2aab80fcc6c
B1607 <145096279+B1607@users.noreply.github.com>
2024-07-10 01:24:53 +0800

    Update get_esm.sh

--- data/get_esm.sh
+++ data/get_esm.sh
@@ -1,7 +1,1 @@
-python exfold.py esm1b_t33_650M_UR50S /mnt/D/jupyter/Melanoma/fasta/Train/neg /mnt/D/jupyter/Melanoma/data/esm1b/train/neg --repr_layers 33 --include per_tok
 
-python exfold.py esm1b_t33_650M_UR50S /mnt/D/jupyter/Melanoma/fasta/Test/neg /mnt/D/jupyter/Melanoma/data/esm1b/test/neg --repr_layers 33 --include per_tok
-
-python exfold.py esm1b_t33_650M_UR50S /mnt/D/jupyter/Melanoma/fasta/Train/pos /mnt/D/jupyter/Melanoma/data/esm1b/train/pos --repr_layers 33 --include per_tok
-
-python exfold.py esm1b_t33_650M_UR50S /mnt/D/jupyter/Melanoma/fasta/Test/pos /mnt/D/jupyter/Melanoma/data/esm1b/test/pos --repr_layers 33 --include per_tok
